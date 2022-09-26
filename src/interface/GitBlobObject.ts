1316f0b8bc33184c34ab966f1b687d0b2ed076fc
shuyu liu <liushuyu6666@gmail.com>
2022-09-26 19:17:10 -0400

    add interface and implement BlobObject

---
+++ src/interface/GitBlobObject.ts
@@ -0,0 +1,25 @@
+export interface GitBlobObject {
+  hash: string;
+
+  prefix: string;
+
+  suffix: string;
+
+  /* absolute location of this object */
+  objectLoc: string;
+
+  // TODO: should be enum
+  type: string;
+
+  /* 
+    This is the Buffer size of the content;
+    Buffer.length != string.length if there are 
+    characters whose value larger than 127
+  */
+  size: number;
+
+  /*
+    content of snapshot of the file
+  */
+  content: string;
+}

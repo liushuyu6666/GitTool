01f75ee969300b86633197847cac10829b4db4ef
shuyu liu <liushuyu6666@gmail.com>
2022-09-26 21:12:13 -0400

    update splitBuffer

--- src/helper/splitBuffer.ts
+++ src/helper/splitBuffer.ts
@@ -1,11 +1,11 @@
-export default function (buffer: Buffer) {
+export default function (buffer: Buffer, delimiter='\u0000') {
   const bufferArray: Buffer[] = [];
 
   let left: Buffer = buffer;
-  while (left.indexOf('\u0000') !== -1) {
-    const delimiter = left.indexOf('\u0000');
-    const former = left.slice(0, delimiter);
-    left = left.slice(delimiter + 1);
+  while (left.indexOf(delimiter) !== -1) {
+    const index = left.indexOf(delimiter);
+    const former = left.slice(0, index);
+    left = left.slice(index + 1);
     bufferArray.push(former);
   }
   bufferArray.push(left);

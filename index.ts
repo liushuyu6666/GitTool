75ac75167cc181f878d16231241916ac994cc562
shuyu liu <liushuyu6666@gmail.com>
2022-09-27 01:06:08 -0400

    use new Git in the index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 // import path from "path";
-import { TreeObject } from "./src/class/TreeObject";
+import { Git } from "./src/class/Git";
 
-
-const rootDir: string = '/Users/shuyuliu/Coding/gitTest/.git';
-// const objectDir: string = path.join(rootDir, 'objects');
+const rootDir: string = '/Users/shuyuliu/Coding/gitTest';
 
 function main() {
-  const treeObject = new TreeObject(rootDir, '3c4e9cd789d88d8d89c1073707c3585e41b0e614');
-  console.log(treeObject.fileEntries)
+  const git = new Git(rootDir);
+  console.log(git.listTypes());
 }
 
 main();

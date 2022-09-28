ead077d889e7c8d204286e9a284ae36e9f5fdba6
shuyu liu <liushuyu6666@gmail.com>
2022-09-28 18:37:33 -0400

    add dotenv

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-// import path from "path";
 import { Git } from "./src/class/Git";
+import * as dotenv from 'dotenv';
+dotenv.config({path: '.env.local'});
 
-const rootDir: string = '/Users/shuyuliu/Coding/gitTest';
+const rootDir: string = process.env.rootDir ?? '';
 
 function main() {
   const git = new Git(rootDir);

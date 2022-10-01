ed6e5cc95d0e2d9bf062a58437d28f3923a811bb
shuyu liu <liushuyu6666@gmail.com>
2022-09-30 23:00:47 -0400

    add .env.localhost.local

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { Git } from "./src/class/Git";
 import * as dotenv from 'dotenv';
+dotenv.config({path: '.env.localhost.local'});
 dotenv.config({path: '.env.local'});
 
 const rootDir: string = process.env.rootDir ?? '';
 
 function main() {
   const git = new Git(rootDir);
-  console.log(JSON.stringify(git.rootTreeHashes, null, 2));
+  console.log(JSON.stringify(git.rootTreeObjects, null, 2));
 }
 
 main();

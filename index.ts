bbe152c0a341e328e4d18d7e236d7669b17225d7
shuyu liu <liushuyu6666@gmail.com>
2022-11-01 22:15:48 -0400

    add ManipulateBuffer class

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Git } from "./src/class/Git";
 import * as dotenv from 'dotenv';
+import { readFileSync } from "fs";
 dotenv.config({path: '.env.localhost.local'});
 dotenv.config({path: '.env.local'});
 
@@ -11,4 +12,37 @@ function main() {
   console.log(JSON.stringify(git.flatTreeObjects, null, 2));
 }
 
-main();
+function pack() {
+  const pack = readFileSync(`/Users/shuyuliu/Coding/gitTest/.git/objects/pack/pack-c86fa0f02f7bc3d0529d483e7376508cacbaf3c7.pack`);
+  const size = pack.length;
+  console.log(size);
+
+  // layer 1: [0, 4): 'PACK'
+  const layer1 = pack.subarray(0, 4);
+  console.log(layer1.toString('utf-8'));
+
+  // layer 2: [4, 8): 'version number
+  const layer2 = pack.subarray(4, 8);
+  console.log(layer2.readUInt32BE());
+
+  // layer 3: [8, 12): 'object number'
+  const layer3 = pack.subarray(8, 12);
+  console.log(layer3.readUInt32BE());
+
+  // layer 4: [12, length - 20)
+  const layer4 = pack.subarray(12, size - 20);
+  let isFirst: boolean = true;
+  let msb: boolean;
+
+  const currEntry = layer4[0];
+  do {
+    msb = 
+  }
+}
+
+const parseChunks = (
+  isFirst: boolean,
+  
+)
+
+pack();
---
+++ src/ManipulateBuffer/ManipulateBuffer.ts
@@ -0,0 +1,74 @@
+export interface ManipulateBufferInterface {
+  buffer: Buffer;
+
+  stock: number;
+
+  capacityOfStock: number;
+
+  cleanTheStockTail(): void;
+
+  assertStockIsByte(): void;
+
+  fill(value: number, validSize: number): void;
+
+
+}
+
+export class ManipulateBuffer implements ManipulateBufferInterface {
+  buffer: Buffer;
+
+  stock: number;
+
+  capacityOfStock: number;
+
+  constructor() {
+    this.buffer = Buffer.alloc(0);
+
+    this.stock = 0; // one byte
+
+    this.capacityOfStock = 8;
+  }
+
+  cleanTheStockTail() {
+    this.stock = this.stock >> this.capacityOfStock << this.capacityOfStock;
+  }
+
+  assertStockIsByte() {
+    if (this.stock > 255) throw new Error('[ManipulateBuffer error]: stock more that one byte');
+  }
+
+  fill(value: number, validSize: number) {
+    if (value > 255 || value < 0) {
+      throw new Error(`[ManipulateBuffer error]: input value is illegal`);
+    }
+    if (validSize > 8 || validSize < 1) {
+      throw new Error(`[ManipulateBuffer error]: input validSize is illegal`)
+    }
+
+    // clear value
+    value = value & 0xff; // ensure only lowest 8 bits are left.
+    value = value >> (8 - validSize) << (8 - validSize) // ensure only valid bits are left
+
+    // bits left after loading
+    const toBeLeft = value << this.capacityOfStock;
+    const newValidSize = Math.min(0, validSize - this.capacityOfStock); // no bit left if it is 0
+
+    this.cleanTheStockTail();
+
+    // load value to the stock
+    const toBeLoaded = value >> 8 - this.capacityOfStock;
+    this.stock = this.stock | toBeLoaded;
+    this.capacityOfStock = Math.max(0, this.capacityOfStock - validSize); // new capacity size
+    
+    this.cleanTheStockTail();
+    this.assertStockIsByte();
+
+    // append stock to the buffer if necessary and build new stock
+    if (this.capacityOfStock === 0) {
+      const uint8 = new Uint8Array([this.stock]);
+      this.buffer = Buffer.concat([this.buffer, uint8], this.buffer.length + uint8.length);
+      this.stock = toBeLeft;
+      this.capacityOfStock = 8 - newValidSize;
+    }
+  }
+}
